a77a940e4e2074f5591dbe0d816e29b80a5483cc
alnova <>
2023-12-16 14:12:22 -0800

    very important update

--- sketch_bonavita_buzzer.ino
+++ sketch_bonavita_buzzer.ino
@@ -1,4 +1,4 @@
-int counter = 0;
+int counter = 0; 
 int adc;
 
 bool onbase_signal;
@@ -10,15 +10,16 @@ enum {
   BUZZ,
   OFFB,
 } state;
+//state can be assigned any of the 4 values of the enum
 
 void setup() {
   // put your setup code here, to run once:
-  Serial.begin(9600);
+  Serial.begin(9600); //baud rate for the uart 
   state = IDLE;
 }
 
-void display_status() {
-  char out[50];
+void display_status() { //void means function doesn't return anything
+  char out[50]; //string of 50 characters
   const char *state_string = (state == IDLE ? "IDLE" :
                               state == HEAT ? "HEAT" :
                               state == BUZZ ? "BUZZ" :
@@ -35,13 +36,13 @@ void loop() {
   delay(1);
 
   adc = analogRead(A0);
-  if (adc > 5) {
+  if (adc > 5) { //scaled value from raw adc reading
     onbase_signal = true;
   } else {
     onbase_signal = false;
   }
 
-  if (digitalRead(3)) {
+  if (digitalRead(3)) { //if the signal on pin3 is positive/true
     heat_signal = true;
   } else {
     heat_signal = false;
